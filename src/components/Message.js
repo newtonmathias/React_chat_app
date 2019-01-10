5a879ec5fd5684ec977c8613026b3f49964fe384
Newton Mathias <newtymathias@gmail.com>
2019-01-10 08:51:39 +0300

    update

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Message extends Component {
-  render() {
-    return (
-      <div className="message">
-        
-      </div>
-    )
-  }
+const Message = (props) => {
+  return (
+    <div className="message">
+      <div className="message-username">{props.username}</div>
+      <div className="message-text">{props.text}</div>
+    </div>
+  )
 }
 
 export default Message
